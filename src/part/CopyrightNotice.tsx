d15a0c66b94c80ecd30258889737989717230dc6
Mark Soderquist <msoderquist@mastercontrol.com>
2026-02-25 13:28:58 -0700

    Start over

--- src/part/CopyrightNotice.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function CopyrightNotice(props) {
-
-	return (
-		<span>&copy; {props.year} Desert Sky Rangers</span>
-	)
-
-}
--- src/part/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import Icons from "../util/Icons";
-
-export default function Loading() {
-
-	return (
-		<div className='page-field'>{Icons.WAIT} loading...</div>
-	)
-
-}
