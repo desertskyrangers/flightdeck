1e86be9a3421e35b54784a79c31dfc1cc908f41b
Mark Soderquist <msoderquist@mastercontrol.com>
2026-02-26 13:22:30 -0700

    Add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
 import React from 'react';
+import {Header} from "./Header";
 
 export function App() {
   return (
     <>
+      <Header/>
       <h1>FlightDeck Parcel React App</h1>
       <p>Edit <code>src/App.tsx</code> to get started!</p>
     </>
