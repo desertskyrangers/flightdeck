076dc5c132fddfe49c9b69e2ea7340ae358d4561
Mark Soderquist <mark@soderquist.net>
2026-02-26 18:51:46 -0700

    Add pages for user management (Register, Reset, Recover, Verify) and dashboards (Public and User) with associated components, services, and styles

---
+++ src/PublicDashboard.tsx
@@ -0,0 +1,137 @@
+import React, {useEffect, useRef, useState} from "react"
+import UserService from "./api/UserService"
+import Notice from "./part/Notice"
+import {useParams} from "react-router"
+import './css/dashboard.css'
+import Times from "./util/Times";
+import Ago from "./part/Ago";
+import Icons from "./util/Icons";
+
+export default function PublicDashboard(props) {
+
+	const idRef = useRef(useParams().id)
+
+	// const navigate = useNavigate()
+
+	const [dashboard, setDashboard] = useState(props.dashboard || {
+		pilotFlightCount: 0,
+		pilotFlightTime: 0
+	})
+
+	const [messages, setMessages] = useState(props.messages || [])
+
+	function clearMessages() {
+		setMessages([])
+	}
+
+	function loadDashboard() {
+		UserService.publicDashboard(idRef.current, (result) => {
+			setDashboard(result.data)
+		}, (failure) => {
+			let messages = failure.messages
+			if (!!!messages) messages = [failure.message]
+			setMessages(messages)
+		})
+	}
+
+	useEffect(loadDashboard, [])
+
+	return (
+		<div className='page-container'>
+			<div className='page-body'>
+				<div className='page-form'>
+					<Notice priority='error' messages={messages} clearMessages={clearMessages}/>
+
+					{!!dashboard.displayName ? <h1>{dashboard.displayName}</h1> : null}
+
+					{!!dashboard.displayName ? <table className='dashboard'>
+						<tbody>
+						{!!dashboard.pilotFlightCount ? <PilotStatsHeader/> : null}
+						{!!dashboard.pilotFlightCount ? <PilotStats count={dashboard.pilotFlightCount} time={dashboard.pilotFlightTime}/> : null}
+						{!!dashboard.observerFlightCount ? <ObserverStatsHeader/> : null}
+						{!!dashboard.observerFlightCount ? <ObserverStats count={dashboard.observerFlightCount} time={dashboard.observerFlightTime}/> : null}
+						</tbody>
+					</table> : null}
+
+					{!!dashboard.aircraftStats ?
+						<table className='stats'>
+							<tbody>
+							{dashboard.aircraftStats.map((craft) => <AircraftRow key={craft.id} value={craft.id} aircraft={craft}/>)}
+							</tbody>
+						</table>
+						: null}
+
+					{!!dashboard.lastPilotFlightTimestamp ? <LastFlight timestamp={dashboard.lastPilotFlightTimestamp}/> : null}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+function PilotStatsHeader() {
+	return (
+		<tr>
+			<td className='dashboard-header'>Flights</td>
+			<td rowSpan={2}>
+				<div className='v-separator'/>
+			</td>
+			<td className='dashboard-header'>Flight Time</td>
+		</tr>
+	)
+}
+
+function PilotStats(props) {
+	return (
+		<tr>
+			<td className='page-metric'>{props.count}</td>
+			<td className='page-metric'>{Times.toSummaryFlightTime(props.time)}</td>
+		</tr>
+	)
+}
+
+function ObserverStatsHeader() {
+	return (
+		<tr>
+			<td className='dashboard-header'>Flights</td>
+			<td rowSpan={2}>
+				<div className='v-separator'/>
+			</td>
+			<td className='dashboard-header'>Observer Time</td>
+		</tr>
+	)
+}
+
+function ObserverStats(props) {
+	return (
+		<tr>
+			<td className='page-metric'>{props.count}</td>
+			<td className='page-metric'>{Times.toSummaryFlightTime(props.time)}</td>
+		</tr>
+	)
+}
+
+function AircraftRow(props) {
+	return (
+		<tr>
+			<td>
+				<span style={{'color':props.aircraft.trimColor, 'backgroundColor':props.aircraft.baseColor,'padding':'0.2rem', 'borderRadius':'0.2rem','display':'inline-block'}}>{Icons.fromAircraftTypeAndStatus(props.aircraft.type, props.aircraft.status)}</span>
+			</td>
+			<td>{props.aircraft.name}</td>
+			<td>{props.aircraft.flightCount}</td>
+			<td>{Times.toSummaryFlightTime(props.aircraft.flightTime)}</td>
+		</tr>
+	)
+}
+
+function LastFlight(props) {
+	return (
+		<table className='metrics'>
+			<tbody>
+			<tr>
+				<td>Last flight:</td>
+				<td><Ago timestamp={props.timestamp}/></td>
+			</tr>
+			</tbody>
+		</table>
+	)
+}
---
+++ src/UserGroups.tsx
@@ -0,0 +1,136 @@
+import "./css/member.css"
+import React, {useEffect, useState} from "react";
+import NoResults from "./part/NoResults";
+import Icons from "./util/Icons";
+import Notice from "./part/Notice";
+import {useNavigate} from "react-router";
+import AppPath from "./AppPath";
+import UserService from "./api/UserService";
+import EntrySelect from "./part/EntrySelect";
+import GroupService from "./api/GroupService";
+import TokenService from "./api/TokenService";
+import {MembershipGroup} from "./part/MembershipGroup";
+import MembershipService from "./api/MembershipService";
+import Loading from "./part/Loading";
+
+export default function UserGroups(props) {
+	const navigate = useNavigate();
+
+	const [groups, setGroups] = useState([])
+	const [memberships, setMemberships] = useState([])
+	const [messages, setMessages] = useState([])
+
+	function clearMessages() {
+		setMessages([])
+	}
+
+	let list;
+	if (!!groups) {
+		list = <MembershipList memberships={memberships} groups={groups} onMembershipRequest={requestMembership} onMemberUpdate={loadMemberships}/>
+	} else {
+		list = <Loading/>
+	}
+
+	function loadGroups() {
+		GroupService.getAvailableGroups((success) => {
+			setGroups(success)
+		}, (failure) => {
+			let messages = failure.messages
+			if (!!!messages) messages = [failure.message]
+			console.log(messages)
+			// if (!!messages) setMessages(messages)
+		})
+	}
+
+	function loadMemberships() {
+		UserService.getMemberships((response) => {
+			setMemberships(response.data)
+		}, (failure) => {
+			let messages = failure.messages
+			if (!!!messages) messages = [failure.message]
+			if (!!messages) setMessages(messages)
+		})
+	}
+
+	function requestMembership(group) {
+		MembershipService.requestMembership(TokenService.getUserId(), group, 'requested', (response) => {
+			setMemberships(response.memberships)
+		}, (failure) => {
+			let messages = failure.messages
+			if (!!!messages) messages = [failure.message]
+			if (!!messages) setMessages(messages)
+		})
+	}
+
+	useEffect(() => loadGroups(), [])
+	useEffect(() => loadMemberships(), [])
+
+	return (
+		<div className='page-container'>
+			<div className='page-body'>
+				<div className='page-form'>
+					{list}
+					<button className='page-action' onClick={() => navigate(AppPath.GROUP + "/new")}>Create a new Group</button>
+					<Notice priority='error' messages={messages} clearMessages={clearMessages}/>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+function JoinRequest(props) {
+
+	const [group, setGroup] = useState(props.group || '')
+
+
+	function requestMembership() {
+		props.onMembershipRequest(group)
+		props.onClose()
+	}
+
+	function getContent() {
+		const hasOptions = !!props.groups && props.groups.length > 0
+
+		return hasOptions ?
+			<EntrySelect id='group'
+									 text='Group'
+									 value={group}
+									 required
+									 defaultValue='unspecified'
+									 onChange={(event) => setGroup(event.target.value)}
+									 fieldActionIcon={Icons.GROUP_ADD}
+									 onFieldAction={requestMembership}>
+				<option key='unspecified' hidden>Select a group</option>
+				{props.groups.map((option) => <option key={option.id} value={option.id}>{option.name}</option>)}
+			</EntrySelect> : <div>No groups available to join</div>
+	}
+
+	return (
+		getContent()
+	)
+
+}
+
+function MembershipList(props) {
+	// Actions
+	const [joinRequest, setJoinRequest] = useState(false)
+
+	function toggleJoinRequest() {
+		setJoinRequest(!joinRequest)
+	}
+
+	let memberships = <NoResults message='No group memberships'/>
+	if (!!props.memberships && props.memberships.length > 0)
+		memberships = props.memberships.map((membership) =>
+			<MembershipGroup key={membership.id}
+											 membership={membership}
+											 onMemberUpdate={props.onMemberUpdate}/>)
+
+	return (
+		<div className='vbox'>
+			<button className='page-action' onClick={() => toggleJoinRequest()}>Join a Group</button>
+			{joinRequest ? <JoinRequest groups={props.groups} onClose={toggleJoinRequest} onMembershipRequest={props.onMembershipRequest}/> : null}
+			{memberships}
+		</div>
+	)
+}
