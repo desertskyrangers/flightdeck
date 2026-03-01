b377dcd73b52453fceb6b04a5b30ff2a627e1be0
Mark Soderquist <mark@soderquist.net>
2026-02-28 18:25:38 -0700

    Initialize Flightdeck project with Vite, React, TypeScript, ESLint, and basic project structure

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  root: "src",
+  build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+  },
+  plugins: [react()],
+})
+
