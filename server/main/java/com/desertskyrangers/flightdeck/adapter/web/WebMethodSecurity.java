f409acfcebd16456c832d4144bd102f0de63ebbe
Mark Soderquist <mark@soderquist.net>
2026-05-29 21:43:23 -0600

    Remove `@EnableGlobalMethodSecurity` annotation in `WebMethodSecurity`

--- server/main/java/com/desertskyrangers/flightdeck/adapter/web/WebMethodSecurity.java
+++ server/main/java/com/desertskyrangers/flightdeck/adapter/web/WebMethodSecurity.java
@@ -1,9 +1,7 @@
 package com.desertskyrangers.flightdeck.adapter.web;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 
 @Configuration
-@EnableGlobalMethodSecurity( prePostEnabled = true, securedEnabled = true, jsr250Enabled = true )
-public class WebMethodSecurity extends GlobalMethodSecurityConfiguration {}
+//@EnableGlobalMethodSecurity( prePostEnabled = true, securedEnabled = true, jsr250Enabled = true )
+public class WebMethodSecurity {}
